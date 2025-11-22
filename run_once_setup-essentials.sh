2bf65e6b2cf15cf27eb1f1255a0acdc68b2631b4
Slava.In <vyacheslav.inyutochkin@gmail.com>
2025-11-22 13:40:50 +0300

    feat: add two setup scripts

---
+++ run_once_setup-essentials.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+log() {
+  sh scripts/log_message.sh "setup-essentials.sh" "${1?}"
+}
+
+# -- Lazygit
+log "Installing Lazygit"
+sudo pacman -Syu --noconfirm lazygit
+log "Lazygit installed"
+
+# -- Docker
+log "Installing Docker"
+sudo pacman -S --noconfirm docker
+log "Docker installed"
+
+# -- Docker Compose
+log "Installing Docker Compose"
+sudo pacman -S --noconfirm docker-compose
+log "Docker Compose installed"
+   
+# -- Docker Configuration
+log "Configuring Docker"
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
+sudo systemctl restart docker
+log "Docker configured"
+
+# -- Lazy Docker
+log "Installing LazyDocker"
+sudo pacman -S --noconfirm lazydocker
+log "LazyDocker installed"
+   
+# -- NodeJS
+log "Installing NodeJS"
+sudo pacman -S --noconfirm nodejs
+log "NodeJS installed"
+
+# -- TypeScript
+log "Installing Typescript"
+sudo pacman -S --noconfirm typescript
+log "Typescript installed"
+
+# -- Rust
+log "Installing Rust"
+sudo pacman -S --noconfirm rust
+log "Rust installed"
+
+# -- Odin
+log "Installing Odin"
+sudo pacman -S --noconfirm odin
+log "Odin installed"
+
+# -- Yarn
+log "Installing Yarn"
+sudo pacman -S --noconfirm yarn
+log "Yarn installed"
+
+# -- Yazi
+log "Installing Yazi"
+sudo pacman -S --noconfirm yazi
+log "Yazi installed"
---
+++ run_once_setup-terminal.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+log() {
+  sh scripts/log_message.sh "setup-terminal.sh" "${1?}"
+}
+
+# -- Alacritty
+log "Installing Alacritty"
+sudo pacman -Syu --noconfirm alacritty
+log "Alacritty installed"
+
+# -- Tmux
+log "Installing Tmux"
+sudo pacman -S --noconfirm tmux
+log "Tmux installed"
+
+# -- Tmux Plugin Manager
+log "Installing Tmux Plugin Manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+log "Tmux Plugign Manager installed"
+
+# -- Fish
+log "Installing Fish"
+sudo pacman -S --noconfirm fish
+chsh -s "$(which fish)"
+log "Fish installed and set as a default shell"
+
+# -- Zoxide
+log "Installing Zoxide"
+sudo pacman -S --noconfirm zoxide
+log "Zoxide installed"
+
+# -- Fzf
+log "Installing Fzf"
+sudo pacman -S --noconfirm fzf
+log "Fzf installed"
+
+# -- Ripgrep
+log "Installing Ripgrep"
+sudo pacman -S --noconfirm ripgrep
+log "Ripgrep installed"
+
+# -- Tree
+log "Installing Tree"
+sudo pacman -S --noconfirm tree
+log "Tree installed"
+
+# -- Xclip
+log "Installing Xclip"
+sudo pacman -S --noconfirm xclip
+log "Xclip installed"
+
+# -- Jq
+log "Installing Jq"
+sudo pacman -S --noconfirm jq
+log "Jq installed"
+
+# -- Btop
+log "Installing Btop"
+sudo pacman -S --noconfirm btop
+log "Btop installed"
+
