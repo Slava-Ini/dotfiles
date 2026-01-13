6fe27a84c45814c5468fb76bd175662250cd747a
Slava.In <vyacheslav.inyutochkin@gmail.com>
2026-01-13 08:07:21 +0300

    docs: cpupower comment

--- run_once_setup-additional-tools.sh
+++ run_once_setup-additional-tools.sh
@@ -5,6 +5,8 @@ log() {
 }
 
 # -- Cpupower
+# Note: needs to start a system.d service additionally and configure frequency `/etc/default/cpupower`
+# See: https://wiki.archlinux.org/title/CPU_frequency_scaling for details
 log "Installing Cpupower"
 sudo pacman -Syu --noconfirm cpupower
 log "Cpupower installed"
