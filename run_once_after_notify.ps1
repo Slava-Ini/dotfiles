95dccdea73128700f3b75516f5782713ee06cd58
Slava.In <vyacheslav.inyutochkin@gmail.com>
2026-01-22 21:22:51 +0300

    feat: rename scripts

--- run_once_after_notify.ps1
+++ run_once_after_notify.ps1
@@ -1,6 +1,6 @@
 function Warn {
     param([string]$Message)
-    & ".\scripts\windows\log_warn.ps1" "notify.ps1" $Message
+    & ".\scripts\windows\log_warning.ps1" "notify.ps1" $Message
 }
 
 Warn "Make sure to bind CapsLock to Escape in SharpKeys and reboot"
