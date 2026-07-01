b2e349e3e675105362e7d1e42144047b5c917909
Slava.In <vyacheslav.inyutochkin@gmail.com>
2026-07-01 16:11:30 +0300

    feat: update emacs config

--- .chezmoitemplates/emacs/init.el
+++ .chezmoitemplates/emacs/init.el
@@ -1,8 +1,13 @@
-(setq visible-bell 1)
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
 (setq help-window-select t)
+(setq inhibit-startup-screen t)
 
 (set-face-attribute 'default nil :height 150)
 
 (menu-bar-mode 0)
 (tool-bar-mode 0)
+(scroll-bar-mode 0)
+
 (delete-selection-mode 1)
+
